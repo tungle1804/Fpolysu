1b7a9ba2de542df31c83ae7959ff9ddeb5862044
namvt <thanhnam.mda93@gmail.com>
2021-06-24 18:46:19 +0700

    add

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,112 +1,158 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react'
-import { useHistory } from 'react-router';
-import { ButtonContext } from '../../service/ButtonContext';
-import UserService from '../../service/UserService';
-import Register from '../Resgiter/index'
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router";
+import { ButtonContext } from "../../service/ButtonContext";
+import UserService from "../../service/UserService";
+import Register from "../Resgiter/index";
 export default function Login() {
-
-    const [user, setUser] = useState([]);
-    const [users, setUsers] = useState([]);
-    let history = useHistory();
-
-
-    const onhandleChange = (e) => {
-        const { name } = e.target;
-        setUser({
-            ...user,
-            [name]: e.target.value
-
-        })
-    }
-//hahahhahaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaas
-    useEffect(() => {
-        UserService.loginUser(user.email).then(res => {
-            setUsers(res.data)
-        })
-    }, [user])
-    const onchangeLogin = () => {
-        // UserService.loginUser(user.email).then((res) => {
-        //     setUsers(res.data)
-        // })
-
-        if (users === null) {
-            alert('email khong ton tai')
-        } else {
-            if (users.password === user.password) {
-                if (users.role == 0) {
-                    history.push('/admin')
-                    localStorage.setItem('email', users.email)
-                } else {
-                    alert('ban da vao trang admin')
-                }
-            } else {
-                console.log(users)
-                alert('password khong chinh xac')
-            }
-        }
-    }
-
-    return (
-        <>
-
-            <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
-                <div className="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
-                    {/* <h1 className="font-bold text-center text-2xl mb-5">Your Logo</h1> */}
-
-                    <div className="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
-                        <div className="px-5 py-7">
-                            <label className="font-semibold text-sm text-gray-600 pb-1 block">E-mail</label>
-                            <input onChange={onhandleChange} name="email" id="email" type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
-                            <label className="font-semibold text-sm text-gray-600 pb-1 block">Password</label>
-                            <input onChange={onhandleChange} name="password" id="password" type="text" className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full" />
-                            <button id="btn_login" onClick={onchangeLogin} className="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block">
-                                <span className="inline-block mr-2">Login</span>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block">
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                </svg>
-                            </button>
-                        </div>
-
-                        <div className="py-5">
-                            <div className="grid grid-cols-2 gap-1">
-                                <div className="text-center sm:text-left whitespace-nowrap">
-                                    <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-top">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
-                                        </svg>
-                                        <span className="inline-block ml-1">Forgot Password</span>
-                                    </button>
-                                </div>
-                                <div className="text-center sm:text-right  whitespace-nowrap">
-                                    <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-bottom	">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
-                                        </svg>
-                                        <span className="inline-block ml-1">Help</span>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="py-5">
-                        <div className="grid grid-cols-2 gap-1">
-                            <div className="text-center sm:text-left whitespace-nowrap">
-                                <button className="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-200 focus:outline-none focus:bg-gray-300 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-4 h-4 inline-block align-text-top">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                                    </svg>
-                                    <span className="inline-block ml-1">Back to your-app.com</span>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="py-6">
+      <div
+        className="
+        flex
+        bg-white
+        rounded-lg
+        shadow-lg
+        overflow-hidden
+        mx-auto
+        max-w-sm
+        lg:max-w-4xl
+        height
+      "
+      >
+        <div
+          className="hidden lg:block lg:w-1/2 bg-cover"
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80')",
+          }}
+        ></div>
+        <div className="w-full p-8 lg:w-1/2">
+          <h1 className="text-2xl font-semibold text-gray-700 text-center">
+            PolyTu
+          </h1>
+          <p className="text-xl text-gray-600 text-center">
+            Wellcome to PolyTu!
+          </p>
+          <a
+            href="..."
+            className="
+            flex
+            items-center
+            justify-center
+            mt-4
+            text-white
+            rounded-lg
+            shadow-md
+            hover:bg-gray-100
+          "
+          >
+            <div className="px-4 py-3 height-48">
+              <svg className="h-6 w-6" viewBox="0 0 40 40">
+                <path
+                  d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                  fill="#FFC107"
+                />
+                <path
+                  d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
+                  fill="#FF3D00"
+                />
+                <path
+                  d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
+                  fill="#4CAF50"
+                />
+                <path
+                  d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
+                  fill="#1976D2"
+                />
+              </svg>
             </div>
-
-
-
-
-        </>
-    )
+            <h1 className="px-4 py-3 w-5/6 text-center text-gray-600 font-bold text-sm">
+              Sign in with Google
+            </h1>
+          </a>
+          <div className="mt-4 flex items-center justify-between">
+            <span className="border-b w-1/5 lg:w-1/4"></span>
+            <a
+              href=".."
+              className="text-xs text-center text-gray-500 uppercase"
+            >
+              or login with email
+            </a>
+            <span className="border-b w-1/5 lg:w-1/4"></span>
+          </div>
+          <div className="mt-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2 float-left">
+              Email Address
+            </label>
+            <input
+              className="
+              bg-gray-200
+              text-gray-700
+              focus:outline-none
+              focus:shadow-outline
+              border border-gray-300
+              rounded
+              py-2
+              px-4
+              block
+              w-full
+              appearance-none
+            "
+              type="email"
+            />
+          </div>
+          <div className="mt-4">
+            <div className="flex justify-between">
+              <label className="block text-gray-700 text-sm font-bold mb-2">
+                Password
+              </label>
+              <a href="forgot-password.html" className="text-xs text-gray-500">
+                Forget Password?
+              </a>
+            </div>
+            <input
+              className="
+              bg-gray-200
+              text-gray-700
+              focus:outline-none
+              focus:shadow-outline
+              border border-gray-300
+              rounded
+              py-2
+              px-4
+              block
+              w-full
+              appearance-none
+            "
+              type="password"
+            />
+          </div>
+          <div className="mt-8">
+            <button
+              className="
+              bg-gray-700
+              text-white
+              font-bold
+              py-2
+              px-4
+              w-full
+              rounded
+              hover:bg-gray-600
+            "
+            >
+              Login
+            </button>
+          </div>
+          <div className="mt-4 flex items-center justify-between">
+            <span className="border-b w-1/5 md:w-1/4"></span>
+            <a href="register.html" className="text-xs text-gray-500 uppercase">
+              or sign up
+            </a>
+            <span className="border-b w-1/5 md:w-1/4"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
